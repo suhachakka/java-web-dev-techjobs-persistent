d90230a45f2d6c01a5c334781419d5a49f420456
suhachakka <suhasinichakka@gmail.com>
2020-01-27 12:46:13 -0600

    Added query1,validations,fields

--- queries.sql
+++ queries.sql
@@ -1,4 +1,10 @@
 ## Part 1: Test it with SQL
+CREATE TABLE Job(
+  job_id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  employer VARCHAR(255),
+  skills VARCHAR(255)
+);
 
 ## Part 2: Test it with SQL
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,25 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class Employer extends AbstractEntity {
 
+@NotBlank
+@Size(min=3,max=20)
+private String location;
 
+public Employer(){}
+
+    public Employer(@NotBlank @Size(min = 3, max = 20) String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
