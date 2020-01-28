d633f2748547c66023c8e8601efbfd038c9fb3bb
suhachakka <suhasinichakka@gmail.com>
2020-01-28 12:30:39 -0600

    created controllers and finished part2

--- queries.sql
+++ queries.sql
@@ -1,8 +1,9 @@
 ## Part 1: Test it with SQL
 --list the columns and their data types in the table.
 DESCRIBE Job;
-
 ## Part 2: Test it with SQL
+-- list the names of the employers in St. Louis City.
+select name from employer where location="st.louis";
 
 ## Part 3: Test it with SQL
 
